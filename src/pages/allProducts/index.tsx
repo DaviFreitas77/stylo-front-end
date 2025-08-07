1de25405ab59f5aa5dacb82cc1fbe3864a8bc2fa
Davi Freitas <davifreitaz999@gmail.com>
2025-08-07 20:02:31 -0300

    trocando url

--- src/pages/allProducts/index.tsx
+++ src/pages/allProducts/index.tsx
@@ -1,12 +1,14 @@
-
+import { useLocation } from "react-router-dom"
 import Card from "@/components/Card";
 import type { CardProps } from "@/components/Card";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer/indesx";
+
 import { useEffect } from "react";
 export default function AllProduct() {
 
-
+    const location = useLocation()
+    const products = location.state?.products || [];
    
     useEffect(()=>{
         window.scrollTo(0,0)
@@ -16,12 +18,11 @@ export default function AllProduct() {
             <Header/>
       
            <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 grid-cols-2 gap-2">
-                {/* {products && products.map((item: CardProps) => {
+                {products && products.map((item: CardProps) => {
                     return (
                         <Card name={item.name} image={item.image} price={Number(item.price)} lastPrice={Number(item.lastPrice)} id={item.id} />
                     )
-                })} */}
-                a
+                })}
             </div>
             <Footer/>
         </div>
